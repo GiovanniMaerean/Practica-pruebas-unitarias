d72d20439ce8686186a82109e27f36348d7ab930
albertm15 <155188920+albertm15@users.noreply.github.com>
2023-12-29 20:56:58 +0100

    exception package

---
+++ src/exception/NifFormatException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class NifFormatException extends Exception{
+    public NifFormatException(){}
+
+    public NifFormatException(String message){
+        super(message);
+    }
+}
