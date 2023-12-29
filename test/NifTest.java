a21311f5b00ef04c428641537c8655fda826ef92
Giovanni <giovannimaerean@gmail.com>
2023-12-29 21:08:30 +0100

    nullNifTest

---
+++ test/NifTest.java
@@ -0,0 +1,18 @@
+import data.Nif;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class NifTest {
+
+    @Test
+    public void nullNifTest() {
+
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> {
+            new Nif(null);
+        });
+        
+        assertEquals("NIF cannot be null", exception.getMessage());
+    }
+}
