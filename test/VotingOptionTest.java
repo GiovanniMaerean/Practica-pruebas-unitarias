75086becb5b64ee4522fd11249125c1ed82269cb
albertm15 <155188920+albertm15@users.noreply.github.com>
2023-12-29 21:29:23 +0100

    VotingOptionTest

---
+++ test/VotingOptionTest.java
@@ -0,0 +1,18 @@
+
+import data.VotingOption;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class VotingOptionTest {
+
+    @Test
+    public void nullVotingOptionTest(){
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> {
+            new VotingOption(null);
+        });
+
+        assertEquals("Voting option cannot be null", exception.getMessage());
+    }
+}
