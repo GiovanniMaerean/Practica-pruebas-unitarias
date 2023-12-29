62b544fdd90ca76a5dc54e3fb8197416555beb4a
albertm15 <155188920+albertm15@users.noreply.github.com>
2023-12-29 21:21:45 +0100

    PasswordTest

---
+++ test/PasswordTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import data.Password;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class PasswordTest {
+
+    @Test
+    public void nullPasswordTest(){
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> {
+            new Password(null);
+        });
+
+        assertEquals("Password cannot be null", exception.getMessage());
+    }
+}
