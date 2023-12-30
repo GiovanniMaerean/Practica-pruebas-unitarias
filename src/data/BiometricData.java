082f07416c0e4d03ca4e6edf2c8c586c4c936c17
albertm15 <155188920+albertm15@users.noreply.github.com>
2023-12-30 21:01:04 +0100

    BiometricData

--- src/data/BiometricData.java
+++ src/data/BiometricData.java
@@ -1,4 +1,25 @@
 package data;
 
-public class BiometricData {
+final public class BiometricData {
+    SingleBiometricData facialData;
+    SingleBiometricData fingerprintData;
+
+    public BiometricData(SingleBiometricData facialData, SingleBiometricData fingerprintData) throws NullPointerException{
+        if (facialData == null || fingerprintData == null) {
+            throw new NullPointerException("Facial data and fingerprint data cannot be null");
+        }
+
+        this.facialData = facialData;
+
+        this.fingerprintData = fingerprintData;
+    }
+
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BiometricData biometricData = (BiometricData) o;
+        return facialData.equals(biometricData.facialData) && fingerprintData.equals(biometricData.fingerprintData);
+    }
 }
