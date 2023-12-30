039bfbd9c8df7d4ac074539544567b564a5f1d5c
albertm15 <155188920+albertm15@users.noreply.github.com>
2023-12-30 21:36:04 +0100

    HumanBiometricScanner and HumanBiometricScannerException

---
+++ src/evoting/biometricdataperipheral/HumanBiometricScanner.java
@@ -0,0 +1,11 @@
+package evoting.biometricdataperipheral;
+
+import data.SingleBiometricData;
+import exception.HumanBiometricScanningException;
+
+public interface HumanBiometricScanner {
+    SingleBiometricData scanFaceBiometrics ()
+            throws HumanBiometricScanningException;
+    SingleBiometricData scanFingerprintBiometrics ()
+            throws HumanBiometricScanningException;
+}
---
+++ src/exception/HumanBiometricScanningException.java
@@ -0,0 +1,6 @@
+package exception;
+
+public class HumanBiometricScanningException extends Exception {
+    public HumanBiometricScanningException() {}
+    public HumanBiometricScanningException(String message) {super(message);}
+}
