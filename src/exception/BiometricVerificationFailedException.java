72712ffd8f34950dacdefbc4abd0aba7d2084854
albertm15 <155188920+albertm15@users.noreply.github.com>
2023-12-31 00:03:41 +0100

    BiometricVerificationFailedException

---
+++ src/exception/BiometricVerificationFailedException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class BiometricVerificationFailedException extends Exception {
+    public BiometricVerificationFailedException() {}
+
+    public BiometricVerificationFailedException(String message) {super(message);}
+}
